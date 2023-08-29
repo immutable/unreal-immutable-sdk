fc5e0b32cc9933c86b0fd5c7694585f813b0a4fb
Ben <bemaph@gmail.com>
2023-08-30 07:43:21 +1000

    Temporary fix for broken game-bridge build

--- Source/Immutable/Private/Immutable/ImtblBrowserWidget.cpp
+++ Source/Immutable/Private/Immutable/ImtblBrowserWidget.cpp
@@ -1,4 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "ImtblBrowserWidget.h"
@@ -92,7 +92,8 @@ void UImtblBrowserWidget::SetBrowserContent()
                 return;
             }
             // IMTBL_LOG("Loaded resource: %s", *Resource->GetName())
-            WebBrowserWidget->LoadString(Resource->Data, TEXT("file://immutable/index.html"));
+            //WebBrowserWidget->LoadString(Resource->Data, TEXT("file://immutable/index.html"));
+            WebBrowserWidget->LoadURL(FString::Printf(TEXT("%s%s"), TEXT("file:///"), *FPaths::ConvertRelativePathToFull(FPaths::Combine(FPaths::ProjectContentDir(), TEXT("html"), TEXT("index.html")))));
         }
     }
 #endif
