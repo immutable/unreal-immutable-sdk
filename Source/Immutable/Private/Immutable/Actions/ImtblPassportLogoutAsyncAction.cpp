faf892ad39aa10f233bcf884aa97dd3fbab6063f
YermekG <yermek.garifullanov@immutable.com>
2023-12-19 11:06:49 +1300

    refactor: reformated code to ue standarts

--- Source/Immutable/Private/Immutable/Actions/ImtblPassportLogoutAsyncAction.cpp
+++ Source/Immutable/Private/Immutable/Actions/ImtblPassportLogoutAsyncAction.cpp
@@ -6,42 +6,44 @@
 #include "Immutable/ImmutableSubsystem.h"
 #include "Immutable/Misc/ImtblLogging.h"
 
-UImtblPassportLogoutAsyncAction *
-UImtblPassportLogoutAsyncAction::Logout(UObject *WorldContextObject) {
-  UImtblPassportLogoutAsyncAction *PassportInitBlueprintNode =
-      NewObject<UImtblPassportLogoutAsyncAction>();
-  PassportInitBlueprintNode->WorldContextObject = WorldContextObject;
-  return PassportInitBlueprintNode;
+UImtblPassportLogoutAsyncAction* UImtblPassportLogoutAsyncAction::Logout(UObject* WorldContextObject)
+{
+	UImtblPassportLogoutAsyncAction* PassportInitBlueprintNode = NewObject<UImtblPassportLogoutAsyncAction>();
+
+	PassportInitBlueprintNode->WorldContextObject = WorldContextObject;
+
+	return PassportInitBlueprintNode;
 }
 
-void UImtblPassportLogoutAsyncAction::Activate() {
-  if (!WorldContextObject || !WorldContextObject->GetWorld()) {
-    FString Err = "Logout failed due to missing world or world context object.";
-    IMTBL_WARN("%s", *Err)
-    Failed.Broadcast(Err);
-    return;
-  }
+void UImtblPassportLogoutAsyncAction::Activate()
+{
+	if (!WorldContextObject || !WorldContextObject->GetWorld())
+	{
+		FString Err = "Logout failed due to missing world or world context object.";
+		IMTBL_WARN("%s", *Err)
+		Failed.Broadcast(Err);
+		return;
+	}
 
-  GetSubsystem()->WhenReady(
-      this,
-      &UImtblPassportLogoutAsyncAction::DoLogout); //, /* timoutSec */ 15.0f);
+	GetSubsystem()->WhenReady(this, &UImtblPassportLogoutAsyncAction::DoLogout); //, /* timoutSec */ 15.0f);
 }
 
-void UImtblPassportLogoutAsyncAction::DoLogout(
-    TWeakObjectPtr<UImtblJSConnector> JSConnector) {
-  // Get Passport
-  auto Passport = GetSubsystem()->GetPassport();
-  // Run Logout
-  Passport->Logout(
-      UImmutablePassport::FImtblPassportResponseDelegate::CreateUObject(
-          this, &UImtblPassportLogoutAsyncAction::OnLogoutResponse));
+void UImtblPassportLogoutAsyncAction::DoLogout(TWeakObjectPtr<UImtblJSConnector> JSConnector)
+{
+	// Get Passport
+	auto Passport = GetSubsystem()->GetPassport();
+	// Run Logout
+	Passport->Logout(UImmutablePassport::FImtblPassportResponseDelegate::CreateUObject(this, &UImtblPassportLogoutAsyncAction::OnLogoutResponse));
 }
 
-void UImtblPassportLogoutAsyncAction::OnLogoutResponse(
-    FImmutablePassportResult Result) {
-  if (Result.Success) {
-    LoggedOut.Broadcast(Result.Message);
-  } else {
-    Failed.Broadcast(Result.Message);
-  }
+void UImtblPassportLogoutAsyncAction::OnLogoutResponse(FImmutablePassportResult Result)
+{
+	if (Result.Success)
+	{
+		LoggedOut.Broadcast(Result.Message);
+	}
+	else
+	{
+		Failed.Broadcast(Result.Message);
+	}
 }
