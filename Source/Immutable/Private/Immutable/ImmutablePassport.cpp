cd6ba5b4859ddd0f8779b45d7db23f18910e7bbe
YermekG <yermek.garifullanov@immutable.com>
2024-10-23 11:31:44 +1300

    feat: added signtypeddata method that accepts json string

--- Source/Immutable/Private/Immutable/ImmutablePassport.cpp
+++ Source/Immutable/Private/Immutable/ImmutablePassport.cpp
@@ -190,6 +190,11 @@ void UImmutablePassport::ZkEvmSignTypedDataV4(const FZkEvmSignTypedDataV4Request
 	CallJS(ImmutablePassportAction::ZkEvmSignTypedDataV4, JsonString, ResponseDelegate, FImtblJSResponseDelegate::CreateUObject(this, &UImmutablePassport::OnBridgeCallbackResponse));
 }
 
+void UImmutablePassport::ZkEvmSignTypedDataV4(const FString& JsonString, const FImtblPassportResponseDelegate& ResponseDelegate)
+{
+	CallJS(ImmutablePassportAction::ZkEvmSignTypedDataV4, JsonString, ResponseDelegate, FImtblJSResponseDelegate::CreateUObject(this, &UImmutablePassport::OnBridgeCallbackResponse));
+}
+
 void UImmutablePassport::ConfirmCode(const FString& DeviceCode, const float Interval, const FImtblPassportResponseDelegate& ResponseDelegate)
 {
 	FImmutablePassportCodeConfirmRequestData Data{DeviceCode, Interval};
