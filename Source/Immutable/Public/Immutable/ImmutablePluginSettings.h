a3d96f0dcd2c446783e5e2a1ad50985e6373e783
YermekG <yermek.garifullanov@immutable.com>
2024-11-07 09:31:59 +1300

    chore: added missing commets

--- Source/Immutable/Public/Immutable/ImmutablePluginSettings.h
+++ Source/Immutable/Public/Immutable/ImmutablePluginSettings.h
@@ -7,7 +7,9 @@
 
 
 /**
- * 
+ * ImmutablePluginSettings is a configuration class for the Immutable plugin.
+ * This class contains settings that can be adjusted to control the behavior
+ * of the Immutable plugin within the Unreal Engine environment.
  */
 UCLASS(config = Game, defaultconfig, meta = (DisplayName = "Immutable Plugin Settings"))
 class IMMUTABLE_API UImmutablePluginSettings : public UDeveloperSettings
@@ -15,7 +17,9 @@ class IMMUTABLE_API UImmutablePluginSettings : public UDeveloperSettings
 	GENERATED_BODY()
 
 public:
+	/// The default application configuration class.
+	/// This property holds a reference to a subclass of UApplicationConfig,
+	/// which will be used as the default configuration for the application.
 	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "General")
 	TSubclassOf<UApplicationConfig> DefaultApplicationConfig;
-
 };
