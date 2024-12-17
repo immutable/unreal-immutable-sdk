336d0d0f12c3bb6c739a946c7b08ea8149043e56
ImmutableJeffrey <jeffrey.wong@immutable.com>
2024-12-18 06:54:11 +1000

    refactor: apply formatting

--- Source/Immutable/Private/Immutable/Transak/TransakWebBrowser.cpp
+++ Source/Immutable/Private/Immutable/Transak/TransakWebBrowser.cpp
@@ -25,7 +25,7 @@ FDelegateHandle UTransakWebBrowser::CallAndRegister_OnWhenReady(FOnWhenReady::FD
 
 TSharedRef<SWidget> UTransakWebBrowser::RebuildWidget()
 {
-	if ( IsDesignTime() )
+	if (IsDesignTime())
 	{
 		return SNew(SBox)
 			.HAlign(HAlign_Center)
@@ -78,99 +78,99 @@ void UTransakWebBrowser::HandleOnUrlChanged(const FText& Text)
 
 void UTransakWebBrowser::Load(const FString& WalletAddress, const FString& Email, const FString& ProductsAvailed, const FString& ScreenTitle)
 {
-    if (!WebBrowserWidget.IsValid())
-    {
-        return;
-    }
-    
-    FString UrlToLoad = ComputePath(WalletAddress, Email, ProductsAvailed, ScreenTitle);
+	if (!WebBrowserWidget.IsValid())
+	{
+		return;
+	}
 
-    if (bIsReady)
-    {
+	FString UrlToLoad = ComputePath(WalletAddress, Email, ProductsAvailed, ScreenTitle);
+
+	if (bIsReady)
+	{
 #if (ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 1)
-        WebBrowserWidget->LoadURL(UrlToLoad);
+		WebBrowserWidget->LoadURL(UrlToLoad);
 #endif
-    }
-    else
-    {
-        FDelegateHandle OnWhenReadyHandle = CallAndRegister_OnWhenReady(UTransakWebBrowser::FOnWhenReady::FDelegate::CreateWeakLambda(this, [this, UrlToLoad, OnWhenReadyHandle]()
-        {
+	}
+	else
+	{
+		FDelegateHandle OnWhenReadyHandle = CallAndRegister_OnWhenReady(UTransakWebBrowser::FOnWhenReady::FDelegate::CreateWeakLambda(this, [this, UrlToLoad, OnWhenReadyHandle]()
+		{
 #if (ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 1)
-            WebBrowserWidget->LoadURL(UrlToLoad);
+			WebBrowserWidget->LoadURL(UrlToLoad);
 #else
 			WebBrowserWidget->LoadURL(UrlToLoad);
 #endif
-        	OnWhenReady.Remove(OnWhenReadyHandle);
-        }));
-    }
+			OnWhenReady.Remove(OnWhenReadyHandle);
+		}));
+	}
 }
 
 FString UTransakWebBrowser::ComputePath(const FString& WalletAddress, const FString& Email, const FString& ProductsAvailed, const FString& ScreenTitle)
 {
-    UTransakConfig* TransakConfig = FImmutableUtilities::GetDefaultTransakConfig();
+	UTransakConfig* TransakConfig = FImmutableUtilities::GetDefaultTransakConfig();
 
-    if (!TransakConfig)
-    {
-        return "";
-    }
+	if (!TransakConfig)
+	{
+		return "";
+	}
 
-    FString Path = TransakConfig->GetURL();
-    TArray<FString> QueryParams;
-    
-    QueryParams.Add(FString(TEXT("apiKey=")) + FPlatformHttp::UrlEncode(TransakConfig->GetAPIKey()));
-    QueryParams.Add(FString(TEXT("email=")) + FPlatformHttp::UrlEncode(Email));
-    QueryParams.Add(FString(TEXT("walletAddress=")) + FPlatformHttp::UrlEncode(WalletAddress));
-    QueryParams.Add(FString(TEXT("themeColor=")) + FPlatformHttp::UrlEncode(TransakConfig->GetThemeColor().ToString()));
-    QueryParams.Add(FString(TEXT("isAutoFillUserData=")) + FPlatformHttp::UrlEncode(TransakConfig->IsAutoFillUserData() ? TEXT("true") : TEXT("false")));
-    QueryParams.Add(FString(TEXT("disableWalletAddressForm=")) + FPlatformHttp::UrlEncode(TransakConfig->DisableWalletAddressForm() ? TEXT("true") : TEXT("false")));
+	FString Path = TransakConfig->GetURL();
+	TArray<FString> QueryParams;
 
-    if (!TransakConfig->GetNetwork().IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("network=")) + FPlatformHttp::UrlEncode(TransakConfig->GetNetwork()));
-    }
-    
-    if (!ProductsAvailed.IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("productsAvailed=")) + FPlatformHttp::UrlEncode(ProductsAvailed));    
-    }
-    
-    if (!ScreenTitle.IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("exchangeScreenTitle=")) + FPlatformHttp::UrlEncode(ScreenTitle));    
-    }
-    
-    if (!TransakConfig->GetDefaultCryptoCurrency().IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("defaultCryptoCurrency=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultCryptoCurrency()));
-    }
+	QueryParams.Add(FString(TEXT("apiKey=")) + FPlatformHttp::UrlEncode(TransakConfig->GetAPIKey()));
+	QueryParams.Add(FString(TEXT("email=")) + FPlatformHttp::UrlEncode(Email));
+	QueryParams.Add(FString(TEXT("walletAddress=")) + FPlatformHttp::UrlEncode(WalletAddress));
+	QueryParams.Add(FString(TEXT("themeColor=")) + FPlatformHttp::UrlEncode(TransakConfig->GetThemeColor().ToString()));
+	QueryParams.Add(FString(TEXT("isAutoFillUserData=")) + FPlatformHttp::UrlEncode(TransakConfig->IsAutoFillUserData() ? TEXT("true") : TEXT("false")));
+	QueryParams.Add(FString(TEXT("disableWalletAddressForm=")) + FPlatformHttp::UrlEncode(TransakConfig->DisableWalletAddressForm() ? TEXT("true") : TEXT("false")));
 
-    if (!TransakConfig->GetDefaultFiatAmount().IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("defaultFiatAmount=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultFiatAmount()));
-    }
-    
-    if (!TransakConfig->GetDefaultFiatCurrency().IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("defaultFiatCurrency=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultFiatCurrency()));
-    }
+	if (!TransakConfig->GetNetwork().IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("network=")) + FPlatformHttp::UrlEncode(TransakConfig->GetNetwork()));
+	}
 
-    if (!TransakConfig->GetDefaultPaymentMethod().IsEmpty())
-    {
-        QueryParams.Add(FString(TEXT("defaultPaymentMethod=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultPaymentMethod()));
-    }
+	if (!ProductsAvailed.IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("productsAvailed=")) + FPlatformHttp::UrlEncode(ProductsAvailed));
+	}
 
-    if (TransakConfig->GetCryptoCurrencyList().Num() > 0)
-    {
-        QueryParams.Add(FString(TEXT("cryptoCurrencyList=")) + FPlatformHttp::UrlEncode(FString::Join(TransakConfig->GetCryptoCurrencyList(), TEXT(","))));
-    }
-    
-    if (TransakConfig->GetDisablePaymentMethods().Num() > 0)
-    {
-        QueryParams.Add(FString(TEXT("disablePaymentMethods=")) + FPlatformHttp::UrlEncode(FString::Join(TransakConfig->GetDisablePaymentMethods(), TEXT(","))));
-    }
-    
-    Path += TEXT("?");
-    Path += FString::Join(QueryParams, TEXT("&"));
+	if (!ScreenTitle.IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("exchangeScreenTitle=")) + FPlatformHttp::UrlEncode(ScreenTitle));
+	}
 
-    return Path;
+	if (!TransakConfig->GetDefaultCryptoCurrency().IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("defaultCryptoCurrency=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultCryptoCurrency()));
+	}
+
+	if (!TransakConfig->GetDefaultFiatAmount().IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("defaultFiatAmount=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultFiatAmount()));
+	}
+
+	if (!TransakConfig->GetDefaultFiatCurrency().IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("defaultFiatCurrency=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultFiatCurrency()));
+	}
+
+	if (!TransakConfig->GetDefaultPaymentMethod().IsEmpty())
+	{
+		QueryParams.Add(FString(TEXT("defaultPaymentMethod=")) + FPlatformHttp::UrlEncode(TransakConfig->GetDefaultPaymentMethod()));
+	}
+
+	if (TransakConfig->GetCryptoCurrencyList().Num() > 0)
+	{
+		QueryParams.Add(FString(TEXT("cryptoCurrencyList=")) + FPlatformHttp::UrlEncode(FString::Join(TransakConfig->GetCryptoCurrencyList(), TEXT(","))));
+	}
+
+	if (TransakConfig->GetDisablePaymentMethods().Num() > 0)
+	{
+		QueryParams.Add(FString(TEXT("disablePaymentMethods=")) + FPlatformHttp::UrlEncode(FString::Join(TransakConfig->GetDisablePaymentMethods(), TEXT(","))));
+	}
+
+	Path += TEXT("?");
+	Path += FString::Join(QueryParams, TEXT("&"));
+
+	return Path;
 }
