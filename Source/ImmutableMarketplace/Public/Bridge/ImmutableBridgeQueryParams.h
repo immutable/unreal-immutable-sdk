21b27f531d775694de28d91611cc04b49d102f89
ImmutableJeffrey <jeffrey.wong@immutable.com>
2025-01-30 09:22:46 +1000

    fix: ensure queryparams are set to their correct default values #3552

--- Source/ImmutableMarketplace/Public/Bridge/ImmutableBridgeQueryParams.h
+++ Source/ImmutableMarketplace/Public/Bridge/ImmutableBridgeQueryParams.h
@@ -11,19 +11,19 @@ struct IMMUTABLEMARKETPLACE_API FImmutableBridgeQueryParams
 	GENERATED_BODY()
 
 public:
-	/** The ID of the source blockchain (default is null). */
+	/** The ID of the source blockchain (default: ""). */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Immutable|Marketplace")
 	FString FromChainID;
 
-	/** The address of the token being moved from (default is null). */
+	/** The address of the token being moved from (default: ""). */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Immutable|Marketplace")
 	FString FromTokenAddress;
 
-	/** The ID of the destination blockchain (default is null). */
+	/** The ID of the destination blockchain (default: ""). */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Immutable|Marketplace")
 	FString ToChainID;
 
-	/** The address of the token being moved to (default is null). */
+	/** The address of the token being moved to (default: ""). */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Immutable|Marketplace")
 	FString ToTokenAddress;
 };
