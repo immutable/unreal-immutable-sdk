a68d0b2ebab5aefcac23b902976382c8411aa0bc
ImmutableJeffrey <jeffrey.wong@immutable.com>
2025-02-10 13:37:30 +1000

    feat: add config json

--- Source/ImmutablezkEVMAPI/openapi-generator/batch-files/generate.bat
+++ Source/ImmutablezkEVMAPI/openapi-generator/batch-files/generate.bat
@@ -4,10 +4,11 @@ set OPENAPI_GENERATOR_CLI=openapi-generator-cli
 
 set GENERATOR=-g cpp-ue4
 set TEMPLATE=-t ../template/cpp-ue4
+set CONFIG=-c ../config/cpp-ue4.json
 set INPUT=-i https://imx-openapiv3-mr-sandbox.s3.us-east-2.amazonaws.com/openapi.json
 set OUTPUT=-o ../..
-set ADDITIONAL_PROPERTIES=--additional-properties=modelNamePrefix="API",cppNamespace=ImmutablezkEVMAPI,unrealModuleName=ImmutablezkEVMAPI
+set ADDITIONAL_PROPERTIES=--additional-properties=
 
-%OPENAPI_GENERATOR_CLI% generate %GENERATOR% %TEMPLATE% %INPUT% %OUTPUT% %ADDITIONAL_PROPERTIES%
+%OPENAPI_GENERATOR_CLI% generate %GENERATOR% %TEMPLATE% %CONFIG% %INPUT% %OUTPUT% %ADDITIONAL_PROPERTIES%
 
 pause
--- Source/ImmutablezkEVMAPI/openapi-generator/batch-files/generate.sh
+++ Source/ImmutablezkEVMAPI/openapi-generator/batch-files/generate.sh
@@ -4,8 +4,9 @@ OPENAPI_GENERATOR_CLI=openapi-generator-cli
 
 GENERATOR="-g cpp-ue4"
 TEMPLATE="-t ../template/cpp-ue4"
+CONFIG="-c ../config/cpp-ue4.json"
 INPUT="-i https://imx-openapiv3-mr-sandbox.s3.us-east-2.amazonaws.com/openapi.json"
 OUTPUT="-o ../.."
-ADDITIONAL_PROPERTIES="--additional-properties=modelNamePrefix=API,cppNamespace=ImmutablezkEVMAPI,unrealModuleName=ImmutablezkEVMAPI"
+ADDITIONAL_PROPERTIES="--additional-properties="
 
-$OPENAPI_GENERATOR_CLI generate "$GENERATOR" "$TEMPLATE" "$INPUT" "$OUTPUT" "$ADDITIONAL_PROPERTIES"
+$OPENAPI_GENERATOR_CLI generate "$GENERATOR" "$TEMPLATE" "$CONFIG" "$INPUT" "$OUTPUT" "$ADDITIONAL_PROPERTIES"
